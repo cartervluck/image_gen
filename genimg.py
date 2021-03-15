e9e066fd61434b0dba28bcb932d897c0c24094b0
Carter Luck <cartervluck@gmail.com>
2021-03-15 01:00:38 -0700

    initial commit

---
+++ genimg.py
@@ -0,0 +1,62 @@
+from PIL import Image
+
+center = Image.open("background.png")
+
+ringsFiles = ["ring3.png","ring2.png","ring1.png","center.png"]
+ringSpeeds = [4, 1, -2, 0] # num rotations per year
+
+dayOfYear = int(input("How many days have passed this year? "))
+
+for i in range(len(ringsFiles)):
+  temp = Image.open(ringsFiles[i])
+  temp = temp.rotate(angle=-ringSpeeds[i]*0.6*dayOfYear) # 600 days per year
+  center.paste(temp, (0,0), temp)
+
+center.save("out.png")
+
+'''
+
+Image.rotate(angle, resample=0, expand=0, center=None, translate=None, fillcolor=None)[source]
+Returns a rotated copy of this image. This method returns a copy of this image, rotated the given number of degrees counter clockwise around its centre.
+
+Parameters
+angle – In degrees counter clockwise.
+
+resample – An optional resampling filter. This can be one of PIL.Image.NEAREST (use nearest neighbour), PIL.Image.BILINEAR (linear interpolation in a 2x2 environment), or PIL.Image.BICUBIC (cubic spline interpolation in a 4x4 environment). If omitted, or if the image has mode “1” or “P”, it is set to PIL.Image.NEAREST. See Filters.
+
+expand – Optional expansion flag. If true, expands the output image to make it large enough to hold the entire rotated image. If false or omitted, make the output image the same size as the input image. Note that the expand flag assumes rotation around the center and no translation.
+
+center – Optional center of rotation (a 2-tuple). Origin is the upper left corner. Default is the center of the image.
+
+translate – An optional post-rotate translation (a 2-tuple).
+
+fillcolor – An optional color for area outside the rotated image.
+
+Returns
+An Image object.
+
+This rotates the input image by theta degrees counter clockwise:
+
+from PIL import Image
+
+im = Image.open("hopper.jpg")
+
+# Rotate the image by 60 degrees counter clockwise
+theta = 60
+# Angle is in degrees counter clockwise
+im_rotated = im.rotate(angle=theta)
+
+'''
+
+'''
+from PIL import Image
+
+import numpy as np
+
+img = Image.open("data_mask_1354_2030.png")
+
+background = Image.open("background_1354_2030.png")
+
+background.paste(img, (0, 0), img)
+background.save('how_to_superimpose_two_images_01.png',"PNG")
+'''
---
+++ mapgen.py
@@ -0,0 +1,119 @@
+#create a map :)
+import random
+import time
+import math
+from PIL import Image
+
+while True:
+    x_size = 100
+    y_size = 100
+
+    def checkzeros(a):
+        for j in a:
+            if 0 in j:
+                return True
+        return False
+
+    def printmap(a,coord):
+        #for i in range(1000):
+        #    print("\n")
+        for j in range(len(a)):
+            k = ["X" if (i,j) == coord else "▓" if a[j][i] == 1 else " " for i in range(len(a[j]))]
+            print(" ".join(k))
+            #print(" ".join([str(i) for i in a[j]]))
+
+    arr = [[0 for i in range(x_size)] for j in range(y_size)] #index by y coord then x coord
+
+    border = 20
+
+    x_rand1 = random.randint(border,x_size-border)
+    y_rand1 = random.randint(border,y_size-border) # get random x,y coordinate to seed at
+    x_rand2 = random.randint(border,x_size-border)
+    y_rand2 = random.randint(border,y_size-border) # get random x,y coordinate to seed at
+    x_randMin = min(x_rand1, x_rand2)
+    x_randMax = max(x_rand1, x_rand2)
+    y_randMin = min(y_rand1, y_rand2)
+    y_randMax = max(y_rand1, y_rand2)
+    for j in range(y_randMin, y_randMax+1):
+        for i in range(x_randMin, x_randMax+1):
+            arr[j][i] = 1
+
+    #printmap(arr,(0,0))
+
+    for i in range(20):
+        point = (random.randint(border, x_size-border-1),random.randint(border, y_size-border-1))
+        for j in range(point[1]-int(border/3),point[1]+int(border/3)):
+            for k in range(point[0]-int(border/3),point[0]+int(border/3)):
+                if (point[0]-k)**2 + (point[1]-j)**2 <= (border/3)**2 and arr[j][k] == 1:
+                    arr[j][k] = 2
+        #printmap(arr,point)
+        #time.sleep(0.1)
+
+    vectors = [(0,1),(1,0),(0,-1),(-1,0)]
+    vectors2 = vectors + [(1,1),(1,-1),(-1,-1),(-1,1)]
+    lookat = (x_randMin, y_randMin-1)
+    currentdir = 1
+
+    #printmap(arr,lookat)
+    #time.sleep(1)
+
+    while checkzeros(arr):
+        if arr[lookat[1]][lookat[0]] == 0:
+            pick = vectors[(1+currentdir)%4]
+            if random.randint(1,3) != 1:
+                pick = vectors2[random.randint(0,7)]
+            try:
+                if arr[lookat[1] + pick[1]][lookat[0] + pick[0]] == 1:
+                    arr[lookat[1]][lookat[0]] = 1
+                else:
+                    arr[lookat[1]][lookat[0]] = 2
+            except: pass
+        if arr[lookat[1] + vectors[(currentdir + 1) % 4][1]][lookat[0] + vectors[(currentdir + 1) % 4][0]] == 0 or (currentdir == 0 and lookat[1] == y_size-1) or (currentdir == 1 and lookat[0] == x_size-1) or (currentdir == 2 and lookat[1] == 0) or (currentdir == 3 and lookat[0] == 0):
+            currentdir += 1
+            currentdir = currentdir % 4
+        #print("(",lookat[0],",",lookat[1],")")
+        #print("(",lookat[0] + vectors[currentdir][0],",",lookat[1] + vectors[currentdir][1],")")
+        lookat = (min(max(0,lookat[0] + vectors[currentdir][0]), x_size-1), min(max(0,lookat[1] + vectors[currentdir][1]),y_size-1))
+        #printmap(arr,lookat)
+        #time.sleep(0.001)
+
+    printmap(arr,(0,0))
+    '''im = Image.new("RGBA",(x_size,y_size))
+    for j in range(len(arr)):
+        for i in range(len(arr[j])):
+            if arr[j][i] == 1: im.putpixel((i,j),(0,0,0,255))
+            else: im.putpixel((i,j),(255,255,255,255))
+    im.save("poggers.png")'''
+
+    for FILLER in range(5):
+        l2 = [[0 for i in range(2 * x_size)] for j in range(2 * y_size)]
+
+        for i in range(2*y_size):
+            for j in range(2*x_size):
+                corr_x = math.floor(j/2)
+                corr_y = math.floor(i/2)
+                if arr[corr_y][corr_x] == 1:
+                    l2[i][j] = 1
+                else:
+                    count = 0
+                    directions = [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]
+                    directions += [(0,2),(1,2),(2,2),(2,1),(2,0),(2,-1),(2,-2),(1,-2),(0,-2),(-1,-2),(-2,-2),(-2,-1),(-2,0),(-2,1),(-2,2),(-1,2)]
+                    for k in directions:
+                        try:
+                            if arr[math.floor((i + k[1])/2)][math.floor((j + k[0])/2)] == 1:
+                                count += 1
+                        except:
+                            pass
+                    if count >= 7: l2[i][j] = 1
+                    
+        x_size = 2 * x_size
+        y_size = 2 * y_size
+        arr = l2
+
+    im2 = Image.new("RGBA",(x_size,y_size))
+    for j in range(len(l2)):
+        for i in range(len(l2[j])):
+            if l2[j][i] == 1: im2.putpixel((i,j),(0,0,0,255))
+            else: im2.putpixel((i,j),(255,255,255,0))
+    im2.save("imgs/" + "".join([random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890") for z in range(10)]) + ".png")
+
