6d7d963eaff2917185f06e39562d981b8ee86e94
Carter Luck <cartervluck@gmail.com>
2022-02-23 16:39:51 -0800

    generalize interpolation

--- mapgen.py
+++ mapgen.py
@@ -20,12 +20,9 @@ while DOTHING:
         return False
 
     def printmap(a,coord):
-        #for i in range(1000):
-        #    print("\n")
         for j in range(len(a)):
             k = ["X" if (i,j) == coord else "▓" if a[j][i] == 1 else " " for i in range(len(a[j]))]
             print(" ".join(k))
-            #print(" ".join([str(i) for i in a[j]]))
 
     arr = [[0 for i in range(x_size)] for j in range(y_size)] #index by y coord then x coord
 
@@ -43,24 +40,17 @@ while DOTHING:
         for i in range(x_randMin, x_randMax+1):
             arr[j][i] = 1
 
-    #printmap(arr,(0,0))
-
     for i in range(20):
         point = (random.randint(border, x_size-border-1),random.randint(border, y_size-border-1))
         for j in range(point[1]-int(border/3),point[1]+int(border/3)):
             for k in range(point[0]-int(border/3),point[0]+int(border/3)):
                 if (point[0]-k)**2 + (point[1]-j)**2 <= (border/3)**2 and arr[j][k] == 1:
                     arr[j][k] = 2
-        #printmap(arr,point)
-        #time.sleep(0.1)
 
     vectors = [(0,1),(1,0),(0,-1),(-1,0)]
     vectors2 = vectors + [(1,1),(1,-1),(-1,-1),(-1,1)]
     lookat = (x_randMin, y_randMin-1)
     currentdir = 1
-
-    #printmap(arr,lookat)
-    #time.sleep(1)
 
     while checkzeros(arr):
         if arr[lookat[1]][lookat[0]] == 0:
@@ -76,19 +66,7 @@ while DOTHING:
         if arr[lookat[1] + vectors[(currentdir + 1) % 4][1]][lookat[0] + vectors[(currentdir + 1) % 4][0]] == 0 or (currentdir == 0 and lookat[1] == y_size-1) or (currentdir == 1 and lookat[0] == x_size-1) or (currentdir == 2 and lookat[1] == 0) or (currentdir == 3 and lookat[0] == 0):
             currentdir += 1
             currentdir = currentdir % 4
-        #print("(",lookat[0],",",lookat[1],")")
-        #print("(",lookat[0] + vectors[currentdir][0],",",lookat[1] + vectors[currentdir][1],")")
         lookat = (min(max(0,lookat[0] + vectors[currentdir][0]), x_size-1), min(max(0,lookat[1] + vectors[currentdir][1]),y_size-1))
-        #printmap(arr,lookat)
-        #time.sleep(0.001)
-
-    #printmap(arr,(0,0))
-    '''im = Image.new("RGBA",(x_size,y_size))
-    for j in range(len(arr)):
-        for i in range(len(arr[j])):
-            if arr[j][i] == 1: im.putpixel((i,j),(0,0,0,255))
-            else: im.putpixel((i,j),(255,255,255,255))
-    im.save("poggers.png")'''
 
     counter = 1
     while len(arr) <= 2**11:
@@ -98,17 +76,14 @@ while DOTHING:
             for j in range(2*x_size):
                 corr_x = math.floor(j/2)
                 corr_y = math.floor(i/2)
-                if arr[corr_y][corr_x] == 1:
-                    l2[i][j] = 1
-                else:
-                    count = 0
-                    for k in directions:
-                        try:
-                            if arr[math.floor((i + k[1])/2)][math.floor((j + k[0])/2)] == 1:
-                                count += 1
-                        except IndexError:
-                            pass
-                    if count >= 7: l2[i][j] = 1
+                count = 0
+                for k in directions:
+                    try:
+                        if arr[math.floor((i + k[1])/2)][math.floor((j + k[0])/2)] == 1:
+                            count += 1
+                    except IndexError:
+                        pass
+                if count >= 7: l2[i][j] = 1
                     
         x_size = 2 * x_size
         y_size = 2 * y_size
